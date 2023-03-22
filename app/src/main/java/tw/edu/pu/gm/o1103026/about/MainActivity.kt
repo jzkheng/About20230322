eda7ea6e2140a2760b6afeb7a49e36f11707c929
student <o1103026@gm.pu.edu.tw>
2023-03-22 14:12:24 +0800

    v.1(邓佳钲)

--- app/src/main/java/tw/edu/pu/gm/o1103026/about/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/gm/o1103026/about/MainActivity.kt
@@ -2,10 +2,18 @@ package tw.edu.pu.gm.o1103026.about
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var txv: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        txv = findViewById(R.id.txv)
+        txv.setOnClickListener({
+            txv.text = "邓佳钲你好"
+
+        })
     }
 }
